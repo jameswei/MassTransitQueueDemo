f6f97d40ed3fe11a7e6817bf509af9533da01d20
wjia <wjia@microsoft.com>
2021-05-07 18:07:15 +0800

    minor changes

--- src/MassTransitQueueDemo/Program.cs
+++ src/MassTransitQueueDemo/Program.cs
@@ -13,11 +13,11 @@ namespace MassTransitQueueDemo
         {
             var configuration = BuildConfiguration();
 
-            var busControl = CreateBusControl(configuration);
+            var bus = CreateBusControl(configuration);
 
-            await Task.Factory.StartNew(async () => await busControl.StartAsync());
-
-            var sendEndpoint = await busControl.GetSendEndpoint(new Uri(configuration.QueueUrl));
+            await Task.Factory.StartNew(async () => await bus.StartAsync());
+            // 通过指定的 queue uri 创建 send endpoint
+            var sendEndpoint = await bus.GetSendEndpoint(new Uri(configuration.QueueUrl));
 
             Console.WriteLine("Press a key:");
             Console.WriteLine(" 1 to send a CreateOrder message");
@@ -35,11 +35,11 @@ namespace MassTransitQueueDemo
 
                 if (key.Key == ConsoleKey.NumPad1)
                 {
-                    var message = new CreateOrderMessage() { Id = ++orderId, OrderDate = DateTime.Now };
+                    var message = new CreateOrderMessage() { OrderId = ++orderId, OrderDate = DateTime.Now };
 
                     await sendEndpoint.Send(message);
 
-                    Console.WriteLine($"Order with Id {message.Id} has been sent.");
+                    Console.WriteLine($"Order with Id {message.OrderId} has been sent.");
                 }
                 else if (key.Key == ConsoleKey.NumPad2)
                 {
@@ -51,21 +51,30 @@ namespace MassTransitQueueDemo
 
                     await sendEndpoint.Send(m2);
                 }
+                else if (key.Key == ConsoleKey.Q)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input, press key either 1, 2, or q.");
+                }
 
-            } while (key.Key != ConsoleKey.Q);
+            } while (true);
 
             Console.WriteLine("Stopping...");
 
             await Task.Delay(2000);
 
-            await busControl.StopAsync();
+            await bus.StopAsync();
 
             Console.WriteLine("MassTransit Queue demo stopped.");
         }
 
+        // 加载解析 appsettings.{ENV}.json
         private static Configuration BuildConfiguration()
         {
-            var settings = new Configuration();
+            var appSettings = new Configuration();
 
             var configuration = new ConfigurationBuilder()
                                 .SetBasePath(Environment.CurrentDirectory)
@@ -73,24 +82,27 @@ namespace MassTransitQueueDemo
                                 .AddJsonFile("appSettings.development.json", optional: true)
                                 .Build();
 
-            configuration.Bind(settings);
+            configuration.Bind(appSettings);
 
-            return settings;
+            return appSettings;
         }
 
+        // 创建 MassTransit 的 service bus
         private static IBusControl CreateBusControl(Configuration configuration)
         {
             return
+            // 使用 ASB 作为 transport provider
                 Bus.Factory.CreateUsingAzureServiceBus(configure =>
                 {
                     configure.Host(configuration.ServiceBusConnectionString);
-
                     configure.ReceiveEndpoint(configuration.QueueName, configurator =>
                     {
                         // Since we're not using Topics, or other non-basic ServiceBus features,
                         // we should call the SelectBasicTier method to avoid exceptions thrown by
                         // MassTransit while it tries to enable those features.
+                        // 只使用 ASB queue，所以显式指定 basic-tier，避免 MassTransit 隐式的创建 topic和subscription
                         configurator.SelectBasicTier();
+                        // 指定要消费的 message type
                         configurator.Consumer<CreateOrderMessageConsumer>();
                         configurator.Consumer<CancelOrderMessageConsumer>();
                     });
